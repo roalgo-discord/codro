61f6552a5fe13e26fc2899eb67bdc4a16d2688ef
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2025-03-30 21:38:59 +0300

    feat: add FluentProvider to App

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {FluentProvider, webLightTheme} from "@fluentui/react-components";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document
 .getElementById('root')!)
 .render(
   <StrictMode>
-	  <App />
+	  <FluentProvider theme={webLightTheme}>
+		  <App />
+	  </FluentProvider>
   </StrictMode>,
 )
